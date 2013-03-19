48071627fd4de32a9aca47cf3b2429d0280d733c
Bernhard Häussner <bxt@die-optimisten.net>
2013-03-19 14:01:15 +0100

    Move parser class into own file

--- lib/wptemplates.rb
+++ lib/wptemplates.rb
@@ -1,5 +1,6 @@
 require 'strscan'
 require "wptemplates/version"
+require "wptemplates/parser"
 
 module Wptemplates
   
@@ -29,6 +30,7 @@ module Wptemplates
     def template_of type
       all_templates_of(type).first
     end
+    
   end
   
   class Soup < Array
@@ -65,87 +67,6 @@ module Wptemplates
     def initialize(text)
       @text = text
     end
-  end
-  
-  class Parser
-    
-    def parse(text)
-      @input = StringScanner.new(text)
-      parse_main
-    end
-    
-  protected
-    
-    def parse_main in_template_parameter = false
-      output = Soup.new
-
-      while unit = parse_template || parse_anything(in_template_parameter)
-        output << unit
-      end
-
-      output
-    end
-    
-    def parse_template
-      if @input.scan(/{{/)
-        template = Template.new parse_template_name, parse_template_parameters
-        @input.scan(/}}/) or raise "unclosed template"
-        template
-      else
-        nil
-      end
-    end
-    
-    def parse_anything in_template_parameter = false
-      if in_template_parameter
-        @input.scan(/([^{}|]|{(?!{)|}(?!}))+/) && Text.new(@input.matched)
-      else
-        @input.scan(/([^{]|{(?!{))+/) && Text.new(@input.matched)
-      end
-    end
-    
-    def parse_template_name
-      if @input.scan(/([^|}]|}(?!}))+/)
-        symbolize(@input.matched)
-      else
-        nil
-      end
-    end
-    
-    def parse_template_parameters
-      i = 0
-      h = {}
-      while parsed_named_template_parameter(h) || parse_numeric_template_parameter(h,i) do
-        i += 1
-      end
-      h
-    end
-    
-    def parsed_named_template_parameter(h)
-      if @input.scan(/\|(([^|=}]|}(?!}))+)=/)
-        key = symbolize(@input[1])
-        value = parse_main(true)
-        value[ 0].text.lstrip!
-        value[-1].text.rstrip!
-        h[key] = value
-      else
-        nil
-      end
-    end
-    
-    def parse_numeric_template_parameter(h,i)
-      if @input.scan(/\|/)
-        value = parse_main(true)
-        h[i] = value
-      else
-        nil
-      end
-    end
-    
-    def symbolize string
-      string.strip.gsub(/ /,'_').downcase.to_sym
-    end
-    
   end
   
 end
---
+++ lib/wptemplates/parser.rb
@@ -0,0 +1,85 @@
+require 'strscan'
+require "wptemplates/version"
+
+module Wptemplates
+  class Parser
+    
+    def parse(text)
+      @input = StringScanner.new(text)
+      parse_main
+    end
+    
+  protected
+    
+    def parse_main in_template_parameter = false
+      output = Soup.new
+
+      while unit = parse_template || parse_anything(in_template_parameter)
+        output << unit
+      end
+
+      output
+    end
+    
+    def parse_template
+      if @input.scan(/{{/)
+        template = Template.new parse_template_name, parse_template_parameters
+        @input.scan(/}}/) or raise "unclosed template"
+        template
+      else
+        nil
+      end
+    end
+    
+    def parse_anything in_template_parameter = false
+      if in_template_parameter
+        @input.scan(/([^{}|]|{(?!{)|}(?!}))+/) && Text.new(@input.matched)
+      else
+        @input.scan(/([^{]|{(?!{))+/) && Text.new(@input.matched)
+      end
+    end
+    
+    def parse_template_name
+      if @input.scan(/([^|}]|}(?!}))+/)
+        symbolize(@input.matched)
+      else
+        nil
+      end
+    end
+    
+    def parse_template_parameters
+      i = 0
+      h = {}
+      while parsed_named_template_parameter(h) || parse_numeric_template_parameter(h,i) do
+        i += 1
+      end
+      h
+    end
+    
+    def parsed_named_template_parameter(h)
+      if @input.scan(/\|(([^|=}]|}(?!}))+)=/)
+        key = symbolize(@input[1])
+        value = parse_main(true)
+        value[ 0].text.lstrip!
+        value[-1].text.rstrip!
+        h[key] = value
+      else
+        nil
+      end
+    end
+    
+    def parse_numeric_template_parameter(h,i)
+      if @input.scan(/\|/)
+        value = parse_main(true)
+        h[i] = value
+      else
+        nil
+      end
+    end
+    
+    def symbolize string
+      string.strip.gsub(/ /,'_').downcase.to_sym
+    end
+    
+  end
+end
