894d98712d2e6fdbe01e5884e51370df814c8381
Bernhard Häussner <bxt@die-optimisten.net>
2013-03-20 20:15:20 +0100

    Move ast classes into own file

--- lib/wptemplates.rb
+++ lib/wptemplates.rb
@@ -1,72 +1,12 @@
 require 'strscan'
 require "wptemplates/version"
+require "wptemplates/ast"
 require "wptemplates/parser"
 
 module Wptemplates
   
   def self.parse text
     Parser.new.parse text
-  end
-  
-  module Node
-    def templates
-      []
-    end
-    def all_templates
-      []
-    end
-    def text
-      ""
-    end
-    def templates_of type
-      templates.select{|t| t.name==type}
-    end
-    def template_of type
-      templates_of(type).first
-    end
-    def all_templates_of type
-      all_templates.select{|t| t.name==type}
-    end
-    def template_of type
-      all_templates_of(type).first
-    end
-    
-  end
-  
-  class Soup < Array
-    include Node
-    def templates
-      map(&:templates).flatten(1)
-    end
-    def all_templates
-      map(&:all_templates).flatten(1)
-    end
-    def text
-      map(&:text).join('')
-    end
-  end
-  
-  class Template
-    include Node
-    attr_reader :name, :params
-    def initialize(name, params = {})
-      @name = name
-      @params = params
-    end
-    def templates
-      [self]
-    end
-    def all_templates
-      templates + @params.map{|_,v| v.templates }.flatten(1)
-    end
-  end
-  
-  class Text
-    include Node
-    attr_reader :text
-    def initialize(text)
-      @text = text
-    end
   end
   
 protected
---
+++ lib/wptemplates/ast.rb
@@ -0,0 +1,65 @@
+
+module Wptemplates
+  
+  module Node
+    def templates
+      []
+    end
+    def all_templates
+      []
+    end
+    def text
+      ""
+    end
+    def templates_of type
+      templates.select{|t| t.name==type}
+    end
+    def template_of type
+      templates_of(type).first
+    end
+    def all_templates_of type
+      all_templates.select{|t| t.name==type}
+    end
+    def template_of type
+      all_templates_of(type).first
+    end
+    
+  end
+  
+  class Soup < Array
+    include Node
+    def templates
+      map(&:templates).flatten(1)
+    end
+    def all_templates
+      map(&:all_templates).flatten(1)
+    end
+    def text
+      map(&:text).join('')
+    end
+  end
+  
+  class Template
+    include Node
+    attr_reader :name, :params
+    def initialize(name, params = {})
+      @name = name
+      @params = params
+    end
+    def templates
+      [self]
+    end
+    def all_templates
+      templates + @params.map{|_,v| v.templates }.flatten(1)
+    end
+  end
+  
+  class Text
+    include Node
+    attr_reader :text
+    def initialize(text)
+      @text = text
+    end
+  end
+  
+end
