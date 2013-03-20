2f6e54621151e45291fff0f936beff838a50f1f7
Bernhard Häussner <bxt@die-optimisten.net>
2013-03-20 23:09:46 +0100

    Split main spec into two sub files

--- spec/wptemplates_templates_spec.rb
+++ spec/wptemplates_templates_spec.rb
@@ -2,22 +2,6 @@ require 'spec_helper'
 
 describe Wptemplates do
   
-  it "parses some text into a soup with a single text node" do
-    parsed = Wptemplates.parse(" some text ")
-    expect(parsed.length).to eq(1)
-    expect(parsed.text).to eq(" some text ")
-    expect(parsed[0].text).to eq(" some text ")
-    expect(parsed.templates).to eq([])
-  end
-  
-  it "parses the empty string into a soup with a single empty text node" do
-    parsed = Wptemplates.parse("")
-    expect(parsed.length).to eq(1)
-    expect(parsed.text).to eq("")
-    expect(parsed[0].text).to eq("")
-    expect(parsed.templates).to eq([])
-  end
-  
   it "parses a template without any parameters" do
     parsed = Wptemplates.parse("{{foo}}")
     expect(parsed.length).to eq(1)
---
+++ spec/wptemplates_text_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe Wptemplates do
+  
+  it "parses some text into a soup with a single text node" do
+    parsed = Wptemplates.parse(" some text ")
+    expect(parsed.length).to eq(1)
+    expect(parsed.text).to eq(" some text ")
+    expect(parsed[0].text).to eq(" some text ")
+    expect(parsed.templates).to eq([])
+  end
+  
+  it "parses the empty string into a soup with a single empty text node" do
+    parsed = Wptemplates.parse("")
+    expect(parsed.length).to eq(1)
+    expect(parsed.text).to eq("")
+    expect(parsed[0].text).to eq("")
+    expect(parsed.templates).to eq([])
+  end
+  
+end
