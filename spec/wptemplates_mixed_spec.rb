2ae1fb5dc17bd403609c279cb780961059ccfecd
Bernhard Häussner <bxt@die-optimisten.net>
2013-03-21 06:49:21 +0100

    Add a spec for links in named parameters and proper whitespace removal

--- spec/wptemplates_mixed_spec.rb
+++ spec/wptemplates_mixed_spec.rb
@@ -40,6 +40,21 @@ describe Wptemplates do
     expect(parsed[0].params[0][0].link).to eq("Foo")
   end
   
+  it "parses a link in a template with whitespace removed" do
+    parsed = subject.parse("{{baz|p = [[foo]] }}")
+    expect(parsed.length).to eq(1)
+    expect(parsed.text).to eq("")
+    expect(parsed.templates.length).to eq(1)
+    expect(parsed[0].text).to eq("")
+    expect(parsed[0].name).to eq(:baz)
+    expect(parsed[0].params.keys).to eq([:p])
+    expect(parsed[0].params[:p].text).to eq("foo")
+    expect(parsed[0].params[:p].length).to eq(1)
+    expect(parsed[0].params[:p].links.length).to eq(1)
+    expect(parsed[0].params[:p][0].text).to eq("foo")
+    expect(parsed[0].params[:p][0].link).to eq("Foo")
+  end
+  
   it "parses text, link, letters, link" do
     parsed = subject.parse("bar [[foo]]ny baz")
     expect(parsed.length).to eq(3)
