8543e438aa5aed9dbc802ec1f8ef801efe1d4886
Bernhard Häussner <bxt@die-optimisten.net>
2013-03-24 18:57:37 +0100

    Use the preprocessor for default parse

--- lib/wptemplates.rb
+++ lib/wptemplates.rb
@@ -1,14 +1,20 @@
 require "wptemplates/version"
 require "wptemplates/parser"
+require "wptemplates/preprocessor"
 
 module Wptemplates
   
   def self.parse text
     parser.parse(text)
+    parser.parse(preprocessor.preprocess(text))
   end
   
   def self.parser
     @parser ||= Parser.new
+  end
+  
+  def self.preprocessor
+    @preprocessor ||= Preprocessor.new
   end
   
 end
