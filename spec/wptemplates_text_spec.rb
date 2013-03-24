aea663d224749a0a4834769274aa0a72fd6ced98
Bernhard Häussner <bxt@die-optimisten.net>
2013-03-24 18:40:37 +0100

    Add some specs for html comment removal

--- spec/wptemplates_text_spec.rb
+++ spec/wptemplates_text_spec.rb
@@ -18,4 +18,49 @@ describe Wptemplates do
     expect(parsed.templates).to eq([])
   end
   
+  it "removes normal html comments" do
+    parsed = subject.parse("a<!-- b -->a")
+    expect(parsed.length).to eq(1)
+    expect(parsed.text).to eq("aa")
+    expect(parsed[0].text).to eq("aa")
+    expect(parsed.templates).to eq([])
+    expect(parsed.links).to eq([])
+  end
+  
+  it "removes html comments with excess minus" do
+    parsed = subject.parse("a<!--- b -->a")
+    expect(parsed.length).to eq(1)
+    expect(parsed.text).to eq("aa")
+    expect(parsed[0].text).to eq("aa")
+    expect(parsed.templates).to eq([])
+    expect(parsed.links).to eq([])
+  end
+  
+  it "removes html comments with invalid combinations of <!-> in between" do
+    parsed = subject.parse("d1<!-- d2 -><!-- d3 -- >d4<! -- > d6-->d6")
+    expect(parsed.length).to eq(1)
+    expect(parsed.text).to eq("d1d6")
+    expect(parsed[0].text).to eq("d1d6")
+    expect(parsed.templates).to eq([])
+    expect(parsed.links).to eq([])
+  end
+  
+  it "removes html comments which remove links closing tag" do
+    parsed = subject.parse("[[a|<!-- ]] -->")
+    expect(parsed.length).to eq(1)
+    expect(parsed.text).to eq("[[a|")
+    expect(parsed[0].text).to eq("[[a|")
+    expect(parsed.templates).to eq([])
+    expect(parsed.links).to eq([])
+  end
+  
+  it "removes html comments which remove links opening tag" do
+    parsed = subject.parse("<!-- [[a| -->]]")
+    expect(parsed.length).to eq(1)
+    expect(parsed.text).to eq("[[a|")
+    expect(parsed[0].text).to eq("[[a|")
+    expect(parsed.templates).to eq([])
+    expect(parsed.links).to eq([])
+  end
+  
 end
